ee85c8cf4c829c1e31660c805f7ffb2f845839c3
VasilyeuUV <vasilyeu.uv@mail.ru>
2024-05-27 19:55:31 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Avalonia.AngelSix.LoudnesMeter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
